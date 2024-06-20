361aa450b1b2b74be98a608bc7b4e68c75412a92
imrahn <35425886+ImrahnF@users.noreply.github.com>
2024-06-20 15:52:57 -0400

    a

--- EventPlanManagerApp/MainPage.xaml.cs
+++ EventPlanManagerApp/MainPage.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using EventPlanManagerApp.BusinessLogic;
+using Windows.Gaming.Input.ForceFeedback;
 
 namespace EventPlanManagerApp;
 
@@ -6,10 +7,73 @@ public partial class MainPage : ContentPage
 {
 	EventPlanManager eventPlanManager = new EventPlanManager();
 
+	public void populatePicker()
+	{
+		foreach (EventType EventType in Enum.GetValues(typeof(EventType)))
+		{
+			EventTypePicker.Items.Add(EventType.ToString());
+
+        }
+	}
+
     public MainPage()
 	{
 		InitializeComponent();
+		populatePicker();
 	}
+
+    private void AddButton_Clicked(object sender, EventArgs e)
+    {
+		// Add a new class if all 6 fields are not empty
+		int eventPlanID = 0;
+		string eventPlanName = "";
+		DateTime date = DateTime.Now;
+		EventType eventType;
+		float budget = 0.0f;
+		float estCost = 0.0f;
+
+        if (int.TryParse(EventPlanIdEntry.Text, out int newId))
+        {
+            eventPlanID = newId;
+        }
+
+        if (int.TryParse(BudgetEntry.Text, out int newbudget))
+        {
+            budget = newId;
+        }
+
+        if (int.TryParse(EstimatedCostEntry.Text, out int newcost))
+        {
+            estCost = newId;
+        }
+
+        if (!string.IsNullOrEmpty(EventPlanIdEntry.Text))
+		{
+			eventPlanName = EventNameEntry.Text;
+
+        }
+
+		if (EventTypePicker.SelectedIndex == -1)
+		{
+			// nothing is picked
+		} else
+		{
+            string eventTypeName = EventTypePicker.SelectedItem.ToString();
+            if (Enum.TryParse(eventTypeName, out EventType foundType))
+            {
+                eventType = foundType;
+            }
+        }
+
+        if ((eventPlanID != 0) && (eventPlanName != null) && (eventType != null) && (budget != 0.0f) && (estCost != 0.0f) )
+        {
+            EventPlan plan = new EventPlan(eventPlanID, eventPlanName, eventType, budget, estCost);
+            eventPlanManager.AddEventPlan(plan);
+        }
+
+
+
+        }
 }
 
 
